9c7b12808570c3c889ddc961d81fdfff15e05668
Tony Nguyen <nguyen.tony91@gmail.com>
2015-07-13 19:17:55 -0400

    Plus de tests random

--- extension/browseraction/popup.js
+++ extension/browseraction/popup.js
@@ -44,49 +44,6 @@ function CreateHandler(get_action, get_data) {
 		};
 }
 
-window.onload = function () {
-	e = CreateGetElementByID();
-
-	e("btnAddCurrentUrl").onclick = function () {
-		chrome.tabs.query({
-			'active' : true,
-			'lastFocusedWindow' : true
-		}, function (tabs) {
-			var url = tabs[0].url;
-			e("txtYoutubeUrl").value = url;
-			send_to_background('AddUrl', url);
-		});
-	}
-	
-	e('btnShuffle').onclick = CreateHandler(CreateGetFnct('btnShuffle', 'title'));
-	e('btnNext').onclick = CreateHandler(CreateGetFnct('btnNext', 'title'));
-	e('btnPrevious').onclick = CreateHandler(CreateGetFnct('btnPrevious', 'title'));
-	e('btnPlay').onclick = CreateHandler(CreateGetFnct('btnPlay', 'title'));
-	e('btnStop').onclick = CreateHandler(CreateGetFnct('btnStop', 'title'));
-	e('btnAddUrl').onclick = CreateHandler(function () {
-			return 'AddUrl'
-		}, CreateGetFnct('txtYoutubeUrl', 'value'));
-
-	e('btnLoop').onclick = function () {
-		//send_to_background(get_action(), get_data());
-		if (this.checked) {
-			this.parentElement.classList.add('active');
-		} else {
-			this.parentElement.classList.remove('active');
-		}
-		send_to_background('Loop', this.checked);
-	}
-
-	e('btnImport').onclick = function () {
-		importPlaylist();
-	}
-
-	// Force an update of the information in the GUI
-	chrome.extension.sendMessage({
-		action : "GetInfo"
-	});
-}
-
 function GetSongLabel(song) {
 	var str = '';
 	if (song !== undefined) {
@@ -131,39 +88,85 @@ function AddAllSongs(playlist, currentSong) {
 	}
 }
 
-chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
-	if (sender === this || e === undefined)
-		return false;
+if(chrome && chrome.extension) {
+	
+window.onload = function () {
+	e = CreateGetElementByID();
 
-	switch (request.action) {
-	case "DisplayInfo":
-		// TODO (Doesn't work): Manage button displayed.
-		if (request.data.state === "playing") {
-			e("btnPlay").getElementsByTagName("span")[0].className = "glyphicon glyphicon-pause";
-			e("btnPlay").title = "Pause";
-		} else if (request.data.state === "paused" || request.data.state === "stopped") {
-			e("btnPlay").getElementsByTagName("span")[0].className = "glyphicon glyphicon-play";
-			e("btnPlay").title = "Play";
-		}
+	e("btnAddCurrentUrl").onclick = function () {
+		chrome.tabs.query({
+			'active' : true,
+			'lastFocusedWindow' : true
+		}, function (tabs) {
+			var url = tabs[0].url;
+			e("txtYoutubeUrl").value = url;
+			send_to_background('AddUrl', url);
+		});
+	}
+	
+	e('btnShuffle').onclick = CreateHandler(CreateGetFnct('btnShuffle', 'title'));
+	e('btnNext').onclick = CreateHandler(CreateGetFnct('btnNext', 'title'));
+	e('btnPrevious').onclick = CreateHandler(CreateGetFnct('btnPrevious', 'title'));
+	e('btnPlay').onclick = CreateHandler(CreateGetFnct('btnPlay', 'title'));
+	e('btnStop').onclick = CreateHandler(CreateGetFnct('btnStop', 'title'));
+	e('btnAddUrl').onclick = CreateHandler(function () {
+			return 'AddUrl'
+		}, CreateGetFnct('txtYoutubeUrl', 'value'));
 
-		AddAllSongs(request.data.playlist, request.data.current);
-		var status = request.data.state;
-		if (request.data.current !== undefined) {
-			status = status + ". Current song: " + GetSongLabel(request.data.current);
+	e('btnLoop').onclick = function () {
+		//send_to_background(get_action(), get_data());
+		if (this.checked) {
+			this.parentElement.classList.add('active');
+		} else {
+			this.parentElement.classList.remove('active');
 		}
-		e('txtYoutubeUrl').value = status;
-		break;
-	case "ChangeSongPlaying":
-		break;
-	case "ChangePlayStatus":
-		break;
-	case "OnError":
-		//alert(event.data.data);
-		break;
+		send_to_background('Loop', this.checked);
 	}
 
-	return true;
-});
+	e('btnImport').onclick = function () {
+		importPlaylist();
+	}
+
+	// Force an update of the information in the GUI
+	chrome.extension.sendMessage({
+		action : "GetInfo"
+	});
+}
+
+	chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
+		if (sender === this || e === undefined)
+			return false;
+
+		switch (request.action) {
+		case "DisplayInfo":
+			// TODO (Doesn't work): Manage button displayed.
+			if (request.data.state === "playing") {
+				e("btnPlay").getElementsByTagName("span")[0].className = "glyphicon glyphicon-pause";
+				e("btnPlay").title = "Pause";
+			} else if (request.data.state === "paused" || request.data.state === "stopped") {
+				e("btnPlay").getElementsByTagName("span")[0].className = "glyphicon glyphicon-play";
+				e("btnPlay").title = "Play";
+			}
+
+			AddAllSongs(request.data.playlist, request.data.current);
+			var status = request.data.state;
+			if (request.data.current !== undefined) {
+				status = status + ". Current song: " + GetSongLabel(request.data.current);
+			}
+			e('txtYoutubeUrl').value = status;
+			break;
+		case "ChangeSongPlaying":
+			break;
+		case "ChangePlayStatus":
+			break;
+		case "OnError":
+			//alert(event.data.data);
+			break;
+		}
+
+		return true;
+	});
+}
 
 function importPlaylist() {
 	var f = e('txtFile').files[0];
